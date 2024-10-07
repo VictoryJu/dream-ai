4f75944320f2852e084c6a8a4bc08e1af98dec84
VictoryJu <kob0805@naver.com>
2024-10-08 02:47:29 +0900

    [set]: add colors, animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,7 +43,10 @@ const config: Config = {
         purple: {
           DEFAULT: 'hsl(var(--purple))',
           foreground: 'hsl(var(--purple-foreground))',
+          background: '#D7D7FF',
+          via: '#E8F5FF',
           main: '#7879F1',
+          200: '#F3F5FF',
           300: '#FBFBFE',
         },
         red: {
@@ -51,13 +54,23 @@ const config: Config = {
           foreground: 'hsl(var(--red-foreground))',
           warning: '#FF6969',
         },
+        black: {
+          DEFAULT: 'hsl(var(--black))',
+          foreground: 'hsl(var(--black-foreground))',
+          background: '#3B3D53',
+          main: '#000000',
+          title: '#474747',
+          description: '#3B3D53',
+        },
         gray: {
           DEFAULT: 'hsl(var(--gray))',
           foreground: 'hsl(var(--gray-foreground))',
+          black: '#1B1D1F',
           main: '#F6F6F6',
           100: '#EAEAEA',
           200: '#C7C7C7',
           400: '#565656',
+          500: '#6D737A',
         },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
@@ -77,6 +90,25 @@ const config: Config = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        scaleUp: {
+          '0%': { transform: 'scale(0)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+
+      animation: {
+        scaleUp: 'scaleUp 0.5s ease-in-out forwards',
+        fadeIn: 'fadeIn 0.5s ease-in-out forwards',
+      },
+
+      transitionProperty: {
+        backgroundColor: 'background-color',
       },
     },
   },
