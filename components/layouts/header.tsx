8236bdce08b80dc6737dbbf7ee945edffc5592a8
VictoryJu <kob0805@naver.com>
2024-10-10 02:07:42 +0900

    [refact]: header router button -> Link component

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Image from 'next/image';
-import { Button } from '../ui/button';
 import Link from 'next/link';
 
 const Header = () => {
@@ -22,15 +21,18 @@ const Header = () => {
         <li>자주 묻는 질문</li>
       </ul>
       <div className="flex items-center gap-[30px]">
-        <Button variant="default" className="w-32 h-[60px] rounded-[8px] text-white text-[22px] font-bold">
+        <Link
+          href="/"
+          className="w-32 h-[60px] text-center content-center rounded-[8px] text-white text-[22px] font-bold bg-purple-main hover:bg-purple-main/90"
+        >
           나의 학급
-        </Button>
-        <Button
-          variant="link"
-          className="w-32 h-[60px] py-[16px] px-[24px] rounded-[8px] text-purple-main text-[22px] font-bold"
+        </Link>
+        <Link
+          href="/auth/login"
+          className="w-32 h-[60px] py-[16px] px-[24px] rounded-[8px] text-purple-main text-[22px] font-bold hover:underline"
         >
           로그아웃
-        </Button>
+        </Link>
       </div>
     </nav>
   );
