84d2d60166222de76e690be4a199040fd047f171
gbwlxhd97 <gbwlxhd97@naver.com>
2024-10-18 20:11:38 +0900

    [style]: add book Editor status text

--- app/book/creator/page-navigator.tsx
+++ app/book/creator/page-navigator.tsx
@@ -8,50 +8,65 @@ type PageNavigatorProps = {
 };
 
 const PageNavigator = ({ selectedIndex, onSelect }: PageNavigatorProps) => {
+  /**
+   * status: 'ING','COMPLETE', 'SAVE' , 'NONE'
+   * 추후에 서버를 통해 받는다
+   */
   const mockBook = [
     {
       title: 'Book Title',
       description:
         '대니얼은 자신의 고향 마을에서 친구들과 작별 인사를 나눕니다. “여러분, 나 마왕을 무찌르러 간다니까!" 친구들이 응원하며 미소짓고, 대니얼은 새로운 모험에 대한 기대감으로 가득 차 있습니다.대니얼은 자신의 고향 마을에서 친구들과 작별 인사를 나눕니다. “여러분, 나 마왕을 무찌르러 간다니까!" 친구들이 응원하며 미소짓고, 대니얼은 새로운 모험에 대한 기대감으로 가득 차 있습니다.',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'ING',
     },
     {
       title: 'Book Title',
       description: 'Book Description',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'COMPLETE',
     },
     {
       title: 'Book Title',
       description: 'Book Description',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'SAVE',
     },
     {
       title: 'Book Title',
       description: 'Book Description',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'NONE',
     },
     {
       title: 'Book Title',
       description: 'Book Description',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'COMPLETE',
     },
     {
       title: 'Book Title',
       description: 'Book Description',
       imageUrl: '/images/main/book/book1.webp',
+      status: 'COMPLETE',
     },
   ];
-  const cardStyle = (isActive: boolean) => ({
-    border: isActive ? '2px solid var(--Purple_Main, #7879F1)' : 'none',
-    boxShadow: isActive ? '0px 0.406px 8.115px 0px rgba(0, 0, 0, 0.05)' : 'none',
-  });
 
   return (
     <div className="py-6 bg-purple-200 h-calcScreen overflow-y-auto w-[464px]">
       <div className="flex flex-col gap-5 px-7">
         {mockBook.map((book, idx) => {
-          const { title, description, imageUrl } = book;
+          const { title, description, imageUrl, status } = book;
           const isActive = idx === selectedIndex;
+          // 마지막 인덱스를 계산해 내부에서 조건 처리
+          const indexText = idx === 0 ? '앞표지' : idx === mockBook.length - 1 ? '뒷표지' : `Page ${idx}`;
+          const statusText =
+            status === 'ING' ? '작성중' : status === 'COMPLETE' ? '저장 완료' : status === 'SAVE' ? '임시저장' : '';
+          // 조건부 스타일링
+          const statusClass = cn({
+            'text-red-warning': status === 'ING',
+            'text-orange-main': status === 'SAVE',
+          });
 
           return (
             <BookCard
@@ -62,8 +77,12 @@ const PageNavigator = ({ selectedIndex, onSelect }: PageNavigatorProps) => {
               )}
               onClick={() => onSelect(idx)}
             >
-              <Badge className="absolute top-3 right-3 text-sm font-bold">Page {idx + 1}</Badge>
-              <div className="absolute bottom-3 right-3 text-sm font-bold text-red-warning">작성중</div>
+              <Badge className="absolute top-3 right-3 text-sm font-bold">{indexText}</Badge>
+              {/* {statusText && (
+                <Badge className={cn('absolute bottom-3 right-3 text-sm font-bold', statusClass)}>{statusText}</Badge>
+              )} */}
+              <div className={cn('absolute bottom-3 right-3 text-sm font-bold text-purple-main', statusClass)}>{statusText}</div>
+
               <BookCard.Image imageUrl={imageUrl} width={157} height={157} showOverlayText={true} />
               <BookCard.Content>
                 <BookCard.Title title={title} className="text-[12px]" />
